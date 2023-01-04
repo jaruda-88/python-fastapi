674b11657d3eb69b4ee65d61ef5e7f1a5ce3842a
jaruda-88 <jaruda88@gmail.com>
2023-01-04 13:22:37 +0900

    update

--- main.py
+++ main.py
@@ -11,3 +11,12 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+def run():
+    import uvicorn
+    uvicorn.run(app)
+
+
+if __name__ == "__main__":
+    run()
